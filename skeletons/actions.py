4d76b2c687f1bd0e27e8c46d245b57b519c91c64
cole <cbisaccia@blue-cloak.com>
2021-02-01 21:11:18 -0500

    actuators, actions, percepts

--- skeletons/actions.py
+++ skeletons/actions.py
@@ -1,4 +1,30 @@
-class Action:
+class Directive:
+    """
+        Following the brain-arm-bottle example, a particular directive is the voltage
+        of the electrical impulse at the entry to the arm at a given unit of time.
+    """
     def __init__(self):
         return
-    
+
+
+class Action:
+    """
+        To consider the definition of an action, the following example will be helpful:
+            Imagine a system containing a brain, a body, and a water bottle.
+             - The brain, for our purposes, is the implementation of the agent function. ie) it takes a percept sequence
+                 (memory) and maps it to a given action (electrical/chemical impulse sent to the body)
+             - The body is a collection of actuators, but this action only requires the arm actuator to be completed
+             - The action is characterized by information within the electrical impulses sent to the arm sent over
+                the duration of time the arm is grabbing the water bottle. Within this period of time, the act of
+                'grabbing' can be further
+                broken up into sub-actions, namely moving the arm, closing fingers to grip, and moving arm back. In
+                this way the action can be defined as a time-based sequence of 'directives' which will be interpreted
+                by the arm. The arm actuator is what 'brings to life' those directives within the environment.
+                Therefore it is necessary to provide Actuators with the ability to sub-divide actions into their
+                constituent directives and map them to their 'consequences'.
+
+
+    """
+    def __init__(self):
+        return
+
--- skeletons/actuators.py
+++ skeletons/actuators.py
@@ -1,4 +1,10 @@
 class Actuator:
+    """
+    Following our brain-arm-bottle example defined in actions.py,
+    the actuator filters directives(voltage) at a particular time t. voltage(t) may indicate that the arm needs to
+     contract muscles m1, m2, ..., mk in order to move the center of mass
+    of the arm by delta. the actuator must implement the map (voltage(t)) of the directives to these consequences
+    """
     def __init__(self):
         return
 

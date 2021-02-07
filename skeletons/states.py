e3bfe068ecc2c53948637effdad396070f3e5d60
cole <cbisaccia@blue-cloak.com>
2021-02-07 12:02:47 -0500

    implementation of states

--- skeletons/states.py
+++ skeletons/states.py
@@ -1,3 +1,9 @@
 class State:
-    def __init__(self):
+    """
+    a state should be relevant to a problem, as to not blow up the memory of the program by including
+    extraneous information. for example, if the state is instantiated with a DriveToLocation problem,
+    it will include info relevant to driving, but it wont include the current stock price of google.
+    In this way the state is a subset or subclass of the current environment.
+    """
+    def __init__(self, problem):
         return

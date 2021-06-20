fixing substate calculation for grid2d

examples/test.py
@@ -4,6 +4,10 @@ from skeletons.helpers import manhattan
 from skeletons.actuators import Mover
 from skeletons.sensors import VacuumSensor
 import numpy as np
+
+from skeletons.spaces import Grid2D
+from skeletons.states import State
+
 col = 10
 row = 10
 num_agents = 1
@@ -15,7 +19,7 @@ def init_world():
     agents = [BasicProblemSolver(
         name=('solver' + str(i)),
         environment=world,
-        goal_states=[np.zeros((col, row))],
+        goal_states=[State(grid2d=Grid2D(col, row, grid=np.zeros((col, row))))],
         closed_loop=False,
         actuators=[Mover()],
         sensors=[VacuumSensor],

skeletons/spaces.py
@@ -7,10 +7,10 @@ class Space:
 
 
 class Grid2D(Space):
-    def __init__(self, columns, rows):
+    def __init__(self, columns, rows, grid=None):
         self.c = columns
         self.r = rows
-        self.grid = np.full(shape=(rows, columns), fill_value=0.5)
+        self.grid = grid if isinstance(grid, np.ndarray) else np.full(shape=(rows, columns), fill_value=0.5)
 
     def __repr__(self):
         return Grid2D.__name__